b215a798f357735d82a25f843997bcbd30ca8361
guandeng <guandeng@Hotmail.com>
2024-03-30 23:00:13 +0800

    Optimized alarm exception

---
+++ src/Exception/AlarmException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf Component.
+ */
+
+namespace HyperfComponent\Alarm\Exception;
+
+use Exception;
+
+class AlarmException extends Exception
+{
+}
